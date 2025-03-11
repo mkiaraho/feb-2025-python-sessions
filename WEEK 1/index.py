96b9eed09edff3d7c3e173ed02ec41ab2268dc25
mkiaraho <kiarahomary0@gmail.com>
2025-03-12 00:21:45 +0300

    intro to python

--- WEEK 1/index.py
+++ WEEK 1/index.py
@@ -1,1 +1,22 @@
 print("hello world")
+
+number = 10
+print(number)
+
+
+
+
+
+age = 27 # int datatype
+marks = 90.5 # float datatype
+
+name = "mary kiaraho" #string datatype
+
+
+
+
+
+languages = ["python","html","css" ,3,5.7, "True"]
+print(languages)
+print(languages[2])
+print(languages[-2])
